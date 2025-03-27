88e98922f9c0071144c3560785741bd5e92975a5
Candy <139320965+Candy2004@users.noreply.github.com>
2025-03-27 21:42:09 +0800

    api

---
+++ 3d_transform/src/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const commandMap = {
+    id1: 'python view_viser.py model.model_dir="./output/SCENE01/" scene.scene_path="/data2/lyl/SG/semantic-gaussians/data/car/scene01"',
+    id2: 'command2',
+    id3: 'command3'
+};
+
+export const fetchModelDetails = (id) => {
+    const command = commandMap[id];
+    if (!command) {
+        throw new Error(`No command found for id: ${id}`);
+    }
+
+    console.log("Request payload:", {
+        dir: '/data2/lyl/SG/semantic-gaussians',
+        command: command
+    });
+
+    return axios.post('http://211.71.15.42:3001/api/run-command', {
+        dir: '/data2/lyl/SG/semantic-gaussians',
+        command: command
+    }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
--- 3d_transform/src/views/list.vue
+++ 3d_transform/src/views/list.vue
@@ -1,73 +1,98 @@
 <script setup>
-import { ref } from 'vue';
-import headMenu from '../components/head-menu.vue';
-import card from '../components/card.vue';
+import { ref } from "vue";
+import headMenu from "../components/head-menu.vue";
+import card from "../components/card.vue";
+import { fetchModelDetails } from "../api.js"; // 假设api.js中有这个接口
+
 const modelDatas = ref([
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-},
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-},
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-},
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-},
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-},
-{
-    img: '/img/home/img1.jpg',
-    modelName: 'model',
-    modelId: 'id123',
-    modelBrief:'这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~'
-}
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id1",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id2",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id3",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id4",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id5",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
+  {
+    img: "/img/home/img1.jpg",
+    modelName: "model",
+    modelId: "id6",
+    modelBrief:
+      "这是一个很好的模型，点进来玩玩吧asfewsaddsfgrnhefwrgefvdctadddddddddddd~",
+  },
 ]);
+
+const handleModelClick = async (modelId) => {
+  try {
+    console.log("Clicked modelId:", modelId);
+    const response = await fetchModelDetails(modelId);
+    console.log("Model details:", response);
+    // 跳转到指定页面
+    window.location.href = `http://localhost:8081`;
+  } catch (error) {
+    console.error("Error fetching model details:", error);
+  }
+};
 </script>
 
 <template>
-    <div class="my-list">
-        <div class="el-menu">
-            <headMenu :my_index="2"></headMenu>
-        </div>
-        <div class="cards">
-            <card v-for="modelData in modelDatas" :model="modelData"></card> 
-        </div>
-        
+  <div class="my-list">
+    <div class="el-menu">
+      <headMenu :my_index="2"></headMenu>
     </div>
+    <div class="cards">
+      <card
+        v-for="modelData in modelDatas"
+        :key="modelData.modelId"
+        :model="modelData"
+        @click="handleModelClick(modelData.modelId)"
+      ></card>
+    </div>
+  </div>
 </template>
 
 <style>
-html, body {
-    height: 100%;
-    margin: 0;
+html,
+body {
+  height: 100%;
+  margin: 0;
 }
 .el-menu {
-    margin-top: 3vh; 
-    margin-bottom: 5vh;
+  margin-top: 3vh;
+  margin-bottom: 5vh;
 }
 .cards {
-    display: flex;
-    flex-wrap: wrap; /* 允许换行 */
-    justify-content: flex-start; /* 左对齐 */
-    gap: 0px; /* 卡片之间的间距 */
-    width: 100%;
+  display: flex;
+  flex-wrap: wrap; /* 允许换行 */
+  justify-content: flex-start; /* 左对齐 */
+  gap: 0px; /* 卡片之间的间距 */
+  width: 100%;
 }
 </style>
